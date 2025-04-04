6f09f731c310f55c0371eeb1898c28262bc2dd92
Reloe <Jerome.Bellefroid@gmail.com>
2025-04-04 19:50:40 +0200

    return url link on version request

--- VersionCheck.lua
+++ VersionCheck.lua
@@ -3,15 +3,17 @@ local _, NSI = ... -- Internal namespace
 function NSI:RequestVersionNumber(type, name) -- type == "Addon" or "WA" or "Note"
     if (UnitIsGroupLeader("player") or UnitIsGroupAssistant("player")) then
         NSAPI:Broadcast("NS_VERSION_REQUEST", "RAID", type, name)
+        local url = ""
         if type == "Addon" then
             local ver = C_AddOns.GetAddOnMetadata(name, "Version") or "0"
-            return {name = NSAPI:GetName("player"), version = ver, duplicate = false}
+            return {name = NSAPI:GetName("player"), version = ver, duplicate = false}, url
         elseif type == "WA" then
             local waData = WeakAuras.GetData(name)
             local ver = -1
             if waData then
                 ver = 0
                 if waData["url"] then
+                    url = waData["url"]
                     ver = tonumber(waData["url"]:match('.*/(%d+)$'))
                 end
             end
@@ -20,11 +22,11 @@ function NSI:RequestVersionNumber(type, name) -- type == "Addon" or "WA" or "Not
                 waData = WeakAuras.GetData(name.." "..i)
                 if waData then duplicate = true break end
             end
-            return {name = NSAPI:GetName("player"), version = ver, duplicate = duplicate}
+            return {name = NSAPI:GetName("player"), version = ver, duplicate = duplicate}, url
         elseif type == "Note" then
             local note = NSAI:GetNote()
             local hashed = C_AddOns.IsAddOnLoaded("MRT") and NSAPI:GetHash(note) or ""
-            return {name = NSAPI:GetName("player"), version = hashed, duplicate = false}
+            return {name = NSAPI:GetName("player"), version = hashed, duplicate = false}, ""
         end
     end
 end
